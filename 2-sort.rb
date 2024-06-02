6508a05e1419844e0ecc5ce6b3f918523dcdf94a
Akinbami Taofeek <tfkakinbami80@yahoo.com>
2024-06-02 21:43:11 +0100

    Fixed code

--- 2-sort.rb
+++ 2-sort.rb
@@ -1,6 +1,6 @@
 ###
 #
-#  Sort integer arguments (ascending)
+#  Sort integer arguments (ascending) 
 #
 ###
 
@@ -11,18 +11,17 @@ ARGV.each do |arg|
 
     # convert to integer
     i_arg = arg.to_i
-
+    
     # insert result at the right position
     is_inserted = false
     i = 0
     l = result.size
     while !is_inserted && i < l do
-        if result[i] < i_arg
-            i += 1
-        else
+        if result[i] >= i_arg
             result.insert(i, i_arg)
             is_inserted = true
-            break
+        else
+            i += 1
         end
     end
     result << i_arg if !is_inserted
